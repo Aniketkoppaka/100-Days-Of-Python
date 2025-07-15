7f93df2ead21f13eb7d12cc5727fa859c0e7c3f5
Aniket Veer Koppaka <146194681+Aniketkoppaka@users.noreply.github.com>
2025-07-15 18:51:13 +0530

    Update main.py

--- FlightDealFinder/main.py
+++ FlightDealFinder/main.py
@@ -73,10 +73,19 @@ for destination in sheet_data:
         print(f"Cheapest indirect flight price is: {CURRENCY}{cheapest_flight.price}")
 
     # If the new flight is cheaper than what’s in the sheet, send an SMS alert
-    if isinstance(cheapest_flight.price, (int, float)) and cheapest_flight.price < destination["lowestPrice"]:
-        print(f"Lower price flight found to {destination['city']}!")
-        notification_manager.send_sms(
-            message_body=f"Low price alert! Only {CURRENCY}{cheapest_flight.price} to fly "
-                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        )
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        # Customise the message depending on the number of stops
+        if cheapest_flight.stops == 0:
+            message = f"Low price alert! Only {CURRENCY} {cheapest_flight.price} to fly direct "\
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+                      f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        else:
+            message = f"Low price alert! Only {CURRENCY} {cheapest_flight.price} to fly "\
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+                      f"with {cheapest_flight.stops} stop(s) "\
+                      f"departing on {cheapest_flight.out_date} and returning on {cheapest_flight.return_date}."
+
+        print(f"Check your email. Lower price flight found to {destination['city']}!")
+
+        # Send emails to everyone on the list
+        notification_manager.send_emails(email_list=customer_email_list, email_body=message)
