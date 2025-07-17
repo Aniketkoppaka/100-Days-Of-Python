7a4f388b7130e2c75bd3084c89290832f73f9925
Aniket Veer Koppaka <146194681+Aniketkoppaka@users.noreply.github.com>
2025-07-17 20:44:33 +0530

    Update main.py

--- BillboardHot100/main.py
+++ BillboardHot100/main.py
@@ -9,7 +9,7 @@ date = input("Which year do you want to travel to? Type the date in this format 
 
 # ---- SCRAPE BILLBOARD ----
 header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
 billboard_url = "https://www.billboard.com/charts/hot-100/" + date
 response = requests.get(url=billboard_url, headers=header)
