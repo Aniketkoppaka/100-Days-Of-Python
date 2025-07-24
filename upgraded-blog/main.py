948999db5bcf2bef838105c13142ec2bd368df1b
Aniket Veer Koppaka <146194681+Aniketkoppaka@users.noreply.github.com>
2025-07-24 18:30:11 +0530

    Update main.py

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
+import smtblib
 import requests
 
 posts = requests.get("https://api.npoint.io/c5e814f9e698f0694608").json()
+EMAIL = "EMAIL"
+PASSWORD = "PASSWORD"
+SMPT = "SMTP"
 
 app = Flask(__name__)
 
@@ -17,10 +21,7 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
     
@@ -31,6 +32,13 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP(SMTP) as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(EMAIL, EMAIL, email_message)
 
 if __name__ == "__main__":
     app.run(debug=True)
