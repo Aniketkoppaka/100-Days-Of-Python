9981c39bb0188dadebf6e9255f37bd58240922e4
Aniket Veer Koppaka <146194681+Aniketkoppaka@users.noreply.github.com>
2025-07-24 18:17:58 +0530

    Update main.py

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -13,18 +13,16 @@ def get_all_posts():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return "<h1>Successfully sent your message</h1>"
     return render_template("contact.html")
-
-@app.route("/form-entry", methods=["POST"])
-def receive_data():
-    data = request.form
-    print(data["name"])
-    print(data["email"])
-    print(data["phone"])
-    print(data["message"])
-    return "<h1>Successfully sent your message</h1>"
     
 @app.route("/post/<int:index>")
 def show_post(index):
